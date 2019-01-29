7448202e48b73ac385230468d9ff6c299705c683
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-01-29 15:02:18 +0100

    Finished the register activity

---
+++ app/src/main/java/com/chatapp/bensadiku/RegisterActivity.kt
@@ -0,0 +1,113 @@
+package com.chatapp.bensadiku
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var currentUid: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        currentUid = FirebaseAuth.getInstance().uid!!
+
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            sendToLogin()
+        }
+
+        selectphoto_button_register.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            selectedPhotoUri = data.data
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+            selectphoto_imageview_register.setImageBitmap(bitmap)
+            selectphoto_button_register.alpha=0f
+        }
+    }
+
+    private fun performRegister() {
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    return@addOnCompleteListener
+                } else {
+                    //sendToLogin()
+                    //finish()
+
+                    uploadImage()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, "FAILED ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun uploadImage() {
+        if(selectedPhotoUri==null) return
+
+        val ref = FirebaseStorage.getInstance().getReference("/images/$currentUid")
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                ref.downloadUrl.addOnSuccessListener {
+
+                    saveUserToFirebaseDB(it.toString())
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveUserToFirebaseDB(profileImageUrl: String) {
+       val ref= FirebaseDatabase.getInstance().getReference("/users/$currentUid")
+        val user=User(currentUid,username_edittext_register.text.toString(),profileImageUrl)
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Successfully created account ",Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun sendToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
+
+
+class User(val uid:String,val username:String,val profileImageUrl:String)
