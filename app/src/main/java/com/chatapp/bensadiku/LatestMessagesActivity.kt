0e576324a91c08f51338ce6a7b0feabbff2df03c
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-01-29 16:34:18 +0100

    Added recycler adapter

---
+++ app/src/main/java/com/chatapp/bensadiku/LatestMessagesActivity.kt
@@ -0,0 +1,57 @@
+package com.chatapp.bensadiku
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LatestMessagesActivity : AppCompatActivity() {
+   private val uid =FirebaseAuth.getInstance().uid
+    private var toolbar: Toolbar?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_messages)
+
+        toolbar=findViewById(R.id.latest_messages_toolbar)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_new_message -> {
+                val intent = Intent(this, NewMessageActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        if(uid==null){
+            val intent= Intent(this,RegisterActivity::class.java)
+            intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/chatapp/bensadiku/NewMessageActivity.kt
@@ -0,0 +1,69 @@
+package com.chatapp.bensadiku
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
+
+class NewMessageActivity : AppCompatActivity() {
+    private var toolbar: Toolbar?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+
+        toolbar=findViewById(R.id.new_message_activity_toolbar)
+        setSupportActionBar(toolbar)
+
+
+        fetchUsers()
+
+    }
+
+    private fun fetchUsers() {
+    val ref=    FirebaseDatabase.getInstance().getReference("/users/")
+        ref.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+                p0.children.forEach {
+
+                    val user = it.getValue(User::class.java)
+                    if(user!=null){
+                        adapter.add(UserItem(user))
+                    }
+
+                }
+                recyclerview_newmessage.adapter = adapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
+    }
+}
+
+class UserItem(val user:User) :Item<ViewHolder>(){
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
+    }
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.username_textview_new_message.text = user.username
+
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageview_new_message)
+
+    }
+
+}
