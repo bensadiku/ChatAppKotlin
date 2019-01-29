0ff06bd8c2bc9510ed370d77ce560bb34d3deb85
bensadiku <43443348+bensadiku@users.noreply.github.com>
2019-01-29 14:15:08 +0100

    Added login activity

---
+++ app/src/main/java/com/chatapp/bensadiku/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.chatapp.bensadiku
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password= password_edittext_login.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password).addOnCompleteListener {
+
+            }
+        }
+
+        back_to_register_textview.setOnClickListener {
+            finish()
+        }
+    }
+}
